7181e2623abbd1f33b2c380caf0b69e63814e35d
Website Of Aarish Adhikary <adhikaryclyde@gmail.com>
2025-03-27 13:05:11 +0530

    Update and rename WSAUpdater.py to Website Of Aarish Adhikary.py

--- Website Of Aarish Adhikary.py
+++ Website Of Aarish Adhikary.py
@@ -1,22 +1,22 @@
-# WSAUpdater
-# Version: 0.0.1a
+# Website Of Aarish Adhikary
+# Version: 45.00.00.365
 #
 #
-# This file is part of the WSABuilds project.
+# This file is part of the Website Of Aarish Adhikary project.
 #
-# WSAUpdater is free software: you can redistribute it and/or modify
+# Website Of Aarish Adhikary is a free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# WSAUpdater is distributed in the hope that it will be useful,
+# Website Of Aarish Adhikary is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with WSAUpdater.  If not, see <https://www.gnu.org/licenses/>.
+# along with Website Of Aarish Adhikary.  If not, see <https://www.gnu.org/licenses/>.
 #
-# Copyright (C) 2023 MustardChef
+# Copyright (C) 2025 Website Of Aarish Adhikary
 #
 #
